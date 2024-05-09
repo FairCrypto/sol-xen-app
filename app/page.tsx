0163ccaae10ef577943ad6d7c62f7b816a9357d9
Nicholas Pettas <npettas@gmail.com>
2024-05-08 22:58:45 -0700

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,168 @@
+"use client";
+import {useCallback, useEffect, useState} from 'react';
+import Image from 'next/image';
+import backgroundImage from '../public/background-image.jpg';
+import {useRouter, useSearchParams} from 'next/navigation'
+
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [totalSupply, setTotalSupply]: [any, any] = useState([]);
+  const {replace} = useRouter()
+  const searchParams = useSearchParams()
+  const [showEthAccount, setShowEthAccount] =
+    useState(searchParams.get('account') === 'ethereum');
+
+  const setAccount = useCallback((account: string) => {
+    const url = new URL(window.location.href)
+    url.searchParams.set('account', account)
+    replace(url.toString())
+  }, [replace])
+
+  const percentOfTotalSupply = (points: number) => {
+    return Math.floor((points / 1_000_000_000 / totalSupply.uiAmount) * 100);
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const leaderboardResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/leaderboard_by_eth`);
+      const leaderboardData = await leaderboardResponse.json();
+      setData(leaderboardData);
+
+      const totalSupplyResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/total_supply`);
+      const totalSupplyData = await totalSupplyResponse.json();
+      setTotalSupply(totalSupplyData);
+    };
+
+    // Call fetchData immediately and then every 30 seconds
+    fetchData();
+    const intervalId = setInterval(fetchData, 30000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <div
+        id="background-image-overlay"
+        style={{
+          display: 'grid',
+          gridGap: '8px',
+          gridTemplateColumns: 'repeat(auto-fit, minmax(400px, auto))',
+        }}
+      >
+        <Image
+          alt="Background image"
+          src={backgroundImage}
+          fill
+          sizes="(min-width: 808px) 50vw, 100vw"
+          style={{
+            objectFit: 'cover',
+          }}
+        />
+      </div>
+
+      {/*<div className="navbar bg-base-100 shadow-xl opacity-85 mb-5">*/}
+      {/*  <a className="btn btn-ghost text-xl">solXEN</a>*/}
+      {/*</div>*/}
+
+      <div className="card w-full md:max-w-screen-xl bg-base-100 shadow-xl opacity-85 md:mt-6">
+        <div className="flex items-center justify-center">
+          <h1 className="text-3xl md:text-5xl mt-3">solXEN Leaderboard</h1>
+        </div>
+
+        <div className="flex items-center justify-center text-xl md:text-3xl mt-3 text-primary">
+          Total
+          supply: {totalSupply.uiAmount ? Intl.NumberFormat("en-US").format(totalSupply.uiAmount) : 'Loading...'}
+        </div>
+
+        <div className="flex items-center justify-end m-4">
+          <div className="join">
+            <input onClick={() => {
+              setShowEthAccount(false)
+              setAccount('solana')
+            }} className="join-item btn btn-sm" type="radio" name="options"
+                   aria-label="Solana" checked={!showEthAccount}/>
+            <input onClick={() => {
+              setShowEthAccount(true)
+              setAccount('ethereum')
+            }} className="join-item btn btn-sm" type="radio" name="options"
+                   aria-label="Ethereum" checked={showEthAccount}
+            />
+          </div>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="table table-fixed md:table-auto table-lg table-zebra">
+            <thead>
+            <tr>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-2 w-10">
+                <span>Rank</span>
+              </th>
+              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <span>Account</span>
+              </th>
+              <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <span>Hashes</span>
+              </th>
+              <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                <span>Super Hashes</span>
+              </th>
+              {!showEthAccount ?
+                <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                  <span>solXEN</span>
+                </th>
+                : null}
+            </tr>
+            </thead>
+            <tbody>
+            {data.map(({rank, solAccount, ethAccount, hashes, superHashes, points}) => (
+              <tr key={rank}>
+                <td className="p-4 pr-0 border-b border-blue-gray-50">
+                  <span color="blue-gray" className="font-bold">{rank}</span>
+                </td>
+                <td className="p-4 border-b border-blue-gray-50 text-xs sm:text-base truncate">
+                <span>
+                  {showEthAccount ? ethAccount : solAccount}
+                </span>
+
+                  <dl className="md:hidden font-normal mt-2">
+                    <div className="flex justify-between">
+                      <dt className="text-sm text-gray-400 mt-1 font-medium">Hashes</dt>
+                      <dd className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(hashes)}</dd>
+                    </div>
+                    <div className="flex justify-between">
+                      <dt className="text-gray-400 text-sm mt-1 font-medium">Super Hashes</dt>
+                      <dd className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(superHashes)}</dd>
+                    </div>
+                    {!showEthAccount ? <div className="flex justify-between">
+                      <dt className="text-gray-400 text-sm mt-1 font-medium">solXEN</dt>
+                      <dd
+                        className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(points / 1_000_000_000)}</dd>
+                    </div> : null}
+                  </dl>
+                </td>
+                <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                  <span className="font-normal">{Intl.NumberFormat("en-US").format(hashes)}</span>
+                </td>
+                <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                  <span className="font-normal">{Intl.NumberFormat("en-US").format(superHashes)}</span>
+                </td>
+                {!showEthAccount ?
+                  <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                    <span className="font-normal">{Intl.NumberFormat("en-US").format(points / 1_000_000_000)}
+                      { percentOfTotalSupply(points) > 0 ?
+                      <small className="text-accent ml-1">({percentOfTotalSupply(points)}%)</small>
+                    : null}
+                    </span>
+                  </td>
+                  : null}
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+import colors from 'tailwindcss/colors';
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    colors: {
+      accent: "bg-accent",
+      primary: "bg-accent",
+    }
+  },
+  plugins: [require('daisyui')],
+  darkMode: 'media',
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+};
+
+export default config;
