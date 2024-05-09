bda2e733fad80d1fb07933f7293f5182a870eecf
Nicholas Pettas <npettas@gmail.com>
2024-05-08 23:39:56 -0700

    fix stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,62 @@
 "use client";
-import {useCallback, useEffect, useState} from 'react';
-import Image from 'next/image';
-import backgroundImage from '../public/background-image.jpg';
-import {useRouter, useSearchParams} from 'next/navigation'
+import React, { useCallback, useEffect, useState, Suspense } from "react";
+import Image from "next/image";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface DataItem {
+  rank: number;
+  solAccount: string;
+  ethAccount: string;
+  hashes: number;
+  superHashes: number;
+  points: number;
+}
 
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<DataItem[]>([]);
   const [totalSupply, setTotalSupply]: [any, any] = useState([]);
-  const {replace} = useRouter()
-  const searchParams = useSearchParams()
-  const [showEthAccount, setShowEthAccount] =
-    useState(searchParams.get('account') === 'ethereum');
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+  const [showEthAccount, setShowEthAccount] = useState(
+    searchParams.get("account") === "ethereum",
+  );
 
-  const setAccount = useCallback((account: string) => {
-    const url = new URL(window.location.href)
-    url.searchParams.set('account', account)
-    replace(url.toString())
-  }, [replace])
+  const setAccount = useCallback(
+    (account: string) => {
+      const url = new URL(window.location.href);
+      url.searchParams.set("account", account);
+      replace(url.toString());
+    },
+    [replace],
+  );
 
   const percentOfTotalSupply = (points: number) => {
     return Math.floor((points / 1_000_000_000 / totalSupply.uiAmount) * 100);
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      const leaderboardResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/leaderboard_by_eth`);
+      const leaderboardResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/leaderboard_by_eth`,
+      );
       const leaderboardData = await leaderboardResponse.json();
+
       setData(leaderboardData);
 
-      const totalSupplyResponse = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/total_supply`);
+      const rows = document.querySelectorAll(".animate-table-row");
+      setTimeout(() => {
+        rows.forEach((row) => row.classList.add("show"));
+      }, 100);
+
+      const totalSupplyResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_ENDPOINT}/v1/solxen/total_supply`,
+      );
       const totalSupplyData = await totalSupplyResponse.json();
       setTotalSupply(totalSupplyData);
     };
 
     // Call fetchData immediately and then every 30 seconds
-    fetchData();
+    fetchData().then();
     const intervalId = setInterval(fetchData, 30000);
 
     // Clear the interval when the component is unmounted
@@ -46,18 +68,20 @@ export default function Home() {
       <div
         id="background-image-overlay"
         style={{
-          display: 'grid',
-          gridGap: '8px',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(400px, auto))',
+          position: "fixed",
+          height: "100%",
+          width: "100%",
+          left: "0",
+          top: "0",
         }}
       >
         <Image
           alt="Background image"
-          src={backgroundImage}
-          fill
+          src="/background-image.jpg"
+          layout="fill"
           sizes="(min-width: 808px) 50vw, 100vw"
           style={{
-            objectFit: 'cover',
+            objectFit: "cover",
           }}
         />
       </div>
@@ -72,93 +96,141 @@ export default function Home() {
         </div>
 
         <div className="flex items-center justify-center text-xl md:text-3xl mt-3 text-primary">
-          Total
-          supply: {totalSupply.uiAmount ? Intl.NumberFormat("en-US").format(totalSupply.uiAmount) : 'Loading...'}
+          Total supply:{" "}
+          {totalSupply.uiAmount
+            ? Intl.NumberFormat("en-US").format(totalSupply.uiAmount)
+            : "Loading..."}
         </div>
 
         <div className="flex items-center justify-end m-4">
-          <div className="join">
-            <input onClick={() => {
-              setShowEthAccount(false)
-              setAccount('solana')
-            }} className="join-item btn btn-sm" type="radio" name="options"
-                   aria-label="Solana" checked={!showEthAccount}/>
-            <input onClick={() => {
-              setShowEthAccount(true)
-              setAccount('ethereum')
-            }} className="join-item btn btn-sm" type="radio" name="options"
-                   aria-label="Ethereum" checked={showEthAccount}
-            />
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="join">
+              <input
+                onClick={() => {
+                  setShowEthAccount(false);
+                  setAccount("solana");
+                }}
+                className="join-item btn btn-sm"
+                type="radio"
+                name="options"
+                aria-label="Solana"
+                checked={!showEthAccount}
+                readOnly={true}
+              />
+              <input
+                onClick={() => {
+                  setShowEthAccount(true);
+                  setAccount("ethereum");
+                }}
+                className="join-item btn btn-sm"
+                type="radio"
+                name="options"
+                aria-label="Ethereum"
+                checked={showEthAccount}
+                readOnly={true}
+              />
+            </div>
+          </Suspense>
         </div>
 
         <div className="overflow-x-auto">
           <table className="table table-fixed md:table-auto table-lg table-zebra">
             <thead>
-            <tr>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-2 w-10">
-                <span>Rank</span>
-              </th>
-              <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                <span>Account</span>
-              </th>
-              <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                <span>Hashes</span>
-              </th>
-              <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                <span>Super Hashes</span>
-              </th>
-              {!showEthAccount ?
-                <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
-                  <span>solXEN</span>
+              <tr>
+                <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-2 w-10">
+                  <span>Rank</span>
                 </th>
-                : null}
-            </tr>
+                <th className="border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                  <span>Account</span>
+                </th>
+                <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                  <span>Hashes</span>
+                </th>
+                <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                  <span>Super Hashes</span>
+                </th>
+                {!showEthAccount ? (
+                  <th className="hidden md:table-cell border-b border-blue-gray-100 bg-blue-gray-50 p-4">
+                    <span>solXEN</span>
+                  </th>
+                ) : null}
+              </tr>
             </thead>
             <tbody>
-            {data.map(({rank, solAccount, ethAccount, hashes, superHashes, points}) => (
-              <tr key={rank}>
-                <td className="p-4 pr-0 border-b border-blue-gray-50">
-                  <span color="blue-gray" className="font-bold">{rank}</span>
-                </td>
-                <td className="p-4 border-b border-blue-gray-50 text-xs sm:text-base truncate">
-                <span>
-                  {showEthAccount ? ethAccount : solAccount}
-                </span>
+              {data.map(
+                (
+                  { rank, solAccount, ethAccount, hashes, superHashes, points },
+                  index,
+                ) => {
+                  return (
+                    <tr key={rank} className={`animate-table-row`}>
+                      <td className="p-4 pr-0 border-b border-blue-gray-50">
+                        <span color="blue-gray" className="font-bold">
+                          {rank}
+                        </span>
+                      </td>
+                      <td className="p-4 border-b border-blue-gray-50 text-xs sm:text-base truncate">
+                        <span>{showEthAccount ? ethAccount : solAccount}</span>
 
-                  <dl className="md:hidden font-normal mt-2">
-                    <div className="flex justify-between">
-                      <dt className="text-sm text-gray-400 mt-1 font-medium">Hashes</dt>
-                      <dd className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(hashes)}</dd>
-                    </div>
-                    <div className="flex justify-between">
-                      <dt className="text-gray-400 text-sm mt-1 font-medium">Super Hashes</dt>
-                      <dd className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(superHashes)}</dd>
-                    </div>
-                    {!showEthAccount ? <div className="flex justify-between">
-                      <dt className="text-gray-400 text-sm mt-1 font-medium">solXEN</dt>
-                      <dd
-                        className="text-gray-400 text-sm mt-1">{Intl.NumberFormat("en-US").format(points / 1_000_000_000)}</dd>
-                    </div> : null}
-                  </dl>
-                </td>
-                <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
-                  <span className="font-normal">{Intl.NumberFormat("en-US").format(hashes)}</span>
-                </td>
-                <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
-                  <span className="font-normal">{Intl.NumberFormat("en-US").format(superHashes)}</span>
-                </td>
-                {!showEthAccount ?
-                  <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
-                    <span className="font-normal">{Intl.NumberFormat("en-US").format(points / 1_000_000_000)}
-                      { percentOfTotalSupply(points) > 0 ?
-                      <small className="text-accent ml-1">({percentOfTotalSupply(points)}%)</small>
-                    : null}
-                    </span>
-                  </td>
-                  : null}
-              </tr>
-            ))}
+                        <dl className="md:hidden font-normal mt-2">
+                          <div className="flex justify-between">
+                            <dt className="text-sm text-gray-400 mt-1 font-medium">
+                              Hashes
+                            </dt>
+                            <dd className="text-gray-400 text-sm mt-1">
+                              {Intl.NumberFormat("en-US").format(hashes)}
+                            </dd>
+                          </div>
+                          <div className="flex justify-between">
+                            <dt className="text-gray-400 text-sm mt-1 font-medium">
+                              Super Hashes
+                            </dt>
+                            <dd className="text-gray-400 text-sm mt-1">
+                              {Intl.NumberFormat("en-US").format(superHashes)}
+                            </dd>
+                          </div>
+                          {!showEthAccount ? (
+                            <div className="flex justify-between">
+                              <dt className="text-gray-400 text-sm mt-1 font-medium">
+                                solXEN
+                              </dt>
+                              <dd className="text-gray-400 text-sm mt-1">
+                                {Intl.NumberFormat("en-US").format(
+                                  points / 1_000_000_000,
+                                )}
+                              </dd>
+                            </div>
+                          ) : null}
+                        </dl>
+                      </td>
+                      <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                        <span className="font-normal">
+                          {Intl.NumberFormat("en-US").format(hashes)}
+                        </span>
+                      </td>
+                      <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                        <span className="font-normal">
+                          {Intl.NumberFormat("en-US").format(superHashes)}
+                        </span>
+                      </td>
+                      {!showEthAccount ? (
+                        <td className="hidden md:table-cell p-4 border-b border-blue-gray-50">
+                          <span className="font-normal">
+                            {Intl.NumberFormat("en-US").format(
+                              points / 1_000_000_000,
+                            )}
+                            {percentOfTotalSupply(points) > 0 ? (
+                              <small className="text-accent ml-1">
+                                ({percentOfTotalSupply(points)}%)
+                              </small>
+                            ) : null}
+                          </span>
+                        </td>
+                      ) : null}
+                    </tr>
+                  );
+                },
+              )}
             </tbody>
           </table>
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
